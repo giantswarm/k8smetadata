6d36b10e9230fb92bb231ea97b4ee804dc06aa16
Thomas Fussell <thomas@giantswarm.io>
2021-12-08 09:48:35 -0600

    fix label (#20)

--- pkg/label/app.go
+++ pkg/label/app.go
@@ -3,3 +3,7 @@ package label
 // AppOperatorWatching is the label added to configmaps by app-operator when
 // it is watching for values changes.
 const AppOperatorWatching = "app-operator.giantswarm.io/watching"
+
+// ClusterAppsOperatorWatching is the label which should be added to Cluster CRs to indicate
+// that cluster-apps-operator should watch it.
+const ClusterAppsOperatorWatching = "cluster-apps-operator.giantswarm.io/watching"
