44cb6c64c9f01e1e6346d0b8a998b28685500673
calvix <vaclav@giantswarm.io>
2022-02-01 09:15:49 +0100

    add-cgroupv1-annotation

--- pkg/annotation/nodepool.go
+++ pkg/annotation/nodepool.go
@@ -3,3 +3,6 @@ package annotation
 // MachinePoolName is the node pool annotation where human-friendly node pool
 // name set by the customer is stored.
 const MachinePoolName = "machine-pool.giantswarm.io/name"
+
+// NodeForceCGroupsV1 is the node pool annotation  that forces cgroups v1 in the operating system
+const NodeForceCGroupsV1 = "node.giantswarm.io/cgroupv1"
