0cfbf266f1ffbdf3a85b05b08a6b5d424164a03c
Christian Bianchi <christian@giantswarm.io>
2022-08-02 17:07:29 +0200

    Update pkg/annotation/cilium.go
    Co-authored-by: Nick Jüttner <nick@giantswarm.io>

--- pkg/annotation/cilium.go
+++ pkg/annotation/cilium.go
@@ -4,7 +4,7 @@ package annotation
 
 // support:
 //   - crd: awsclusters.infrastructure.giantswarm.io
-//     apiversion: v1alpha2
+//     apiversion: v1alpha3
 //     release: Since 18.0.0
 // documentation:
 //   This annotation allows specifying a CIDR to be used by cilium during cluster upgrades from v17 to v18.
