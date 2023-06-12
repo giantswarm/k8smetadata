39ac6b369498b830e1727d27c24a1767540c8c29
Christian Bianchi <christian@giantswarm.io>
2023-06-12 16:40:57 +0200

    Add `cilium.giantswarm.io/ipam-mode` annotation and its values `kubernetes` and `eni`. (#78)

--- pkg/annotation/cilium.go
+++ pkg/annotation/cilium.go
@@ -15,3 +15,19 @@ const CiliumPodCidr = "cilium.giantswarm.io/pod-cidr"
 // Not documented as it's not meant for customers.
 // This annotation allows disabling Cilium's kube proxy implementation.
 const CiliumForceDisableKubeProxyAnnotation = "cilium.giantswarm.io/force-disable-cilium-kube-proxy"
+
+// support:
+//   - crd: clusters.cluster.x-k8s.io
+//     apiversion: v1beta1
+//     release: Since 19.1.0
+//
+// documentation:
+//
+//	This annotation allows specifying what ipam mode to use for cilium. Supported values are "kubernetes", the default
+//	and highly recommended mode and "eni". If annotation is missing, "kubernetes" is assumed.
+//	Please note it is possible to set this annotation only when cluster has release < v19.0.0 and editing of the value
+//	is blocked as soon as v19 upgrade begins.
+const CiliumIpamModeAnnotation = "cilium.giantswarm.io/ipam-mode"
+
+const CiliumIpamModeKubernetes = "kubernetes"
+const CiliumIpamModeENI = "eni"
