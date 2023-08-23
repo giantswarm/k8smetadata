eefcdd12a036794dd2295eba5e3e4f806637fe86
Nick Jüttner <nick@giantswarm.io>
2023-08-23 18:19:11 +0200

    add flatcar alpha channel annotation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added 
+
+- Add `alpha.flatcar.giantswarm.io/release-version` annotation to support flatcar alpha channel releases.
+
 ## [0.21.0] - 2023-06-12
 
 ### Added
---
+++ pkg/annotation/flatcar.go
@@ -0,0 +1,11 @@
+package annotation
+
+// support:
+//   - crd: awsmachinedeployments.infrastructure.giantswarm.io
+//     apiversion: v1alpha3
+//     release: Since 19.1.0
+//
+// documentation:
+//
+//	This annotation is used for configuring flatcar alpha channel release version.
+const FlatcarAlphaReleaseVersion = "alpha.flatcar.giantswarm.io/release-version"
