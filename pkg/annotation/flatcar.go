32662dd355c5a5403e926a3d201e9c4e349deef7
Nick Jüttner <nick@giantswarm.io>
2023-08-24 09:26:39 +0200

    Add flatcar alpha channel annotation (#80)
    * add flatcar alpha channel annotation
    
    * rename
    
    * addressing comments
    
    * Update CHANGELOG.md

---
+++ pkg/annotation/flatcar.go
@@ -0,0 +1,11 @@
+package annotation
+
+// support:
+//   - crd: awsmachinedeployments.infrastructure.giantswarm.io
+//     apiversion: v1alpha3
+//     release: Since 19.1.0
+//
+// documentation:
+//
+//	This annotation is used for selecting a specific flatcar release version for machine deployments.
+const FlatcarReleaseVersion = "alpha.giantswarm.io/flatcar-release-version"
