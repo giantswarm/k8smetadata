c67bbc807dbc5ef807bd726e51c6ceaf86e39315
Andreas Sommer <andreas@giantswarm.io>
2023-10-09 10:25:52 +0200

    Add deletion prevention label

--- pkg/label/general.go
+++ pkg/label/general.go
@@ -1,5 +1,9 @@
 package label
 
+// PreventDeletion can be used on certain resource types to prevent deletion
+// (see https://docs.giantswarm.io/advanced/deletion-prevention/).
+const PreventDeletion = "giantswarm.io/prevent-deletion"
+
 // ServicePriority indicates the relative priority of a cluster.
 // See https://github.com/giantswarm/rfc/tree/main/classify-cluster-priority
 // for details.
