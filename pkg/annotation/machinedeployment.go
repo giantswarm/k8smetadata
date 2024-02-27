862bde2669a8459346978ba3172c9f444b565c47
Nick Jüttner <nick@giantswarm.io>
2024-02-27 10:53:10 +0100

    add machine deployment annotation

--- pkg/annotation/machinedeployment.go
+++ pkg/annotation/machinedeployment.go
@@ -1,5 +1,9 @@
 package annotation
 
+// MachineDeploymentName is the node pool annotation where human-friendly node pool
+// name set by the customer is stored.
+const MachineDeploymentName = "machine-deployment.giantswarm.io/name"
+
 // MachineDeploymentSubnet is used to specify the IPv4 CIDR used for a
 // node pool defined by a MachineDeployment or AWSMachineDeployment CR.
 const MachineDeploymentSubnet = "machine-deployment.giantswarm.io/subnet"
