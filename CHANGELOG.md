9311ca65e6ca784d35b10b0ca8b405b6fa56ee0e
Łukasz Jakimczuk <lukasz.j@giantswarm.io>
2024-05-21 12:28:40 +0200

    Adding label needed for migration

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- Add `app-operator.giantswarm.io/flux-backend` label for Cluster CR.
+
 ## [0.24.0] - 2024-02-27
 
 ### Added
--- pkg/label/app.go
+++ pkg/label/app.go
@@ -1,5 +1,8 @@
 package label
 
+// AppOperatorFluxBackend is the label added to Cluster CR
+const AppOperatorFluxBackend = "app-operator.giantswarm.io/flux-backend"
+
 // AppOperatorWatching is the label added to configmaps by app-operator when
 // it is watching for values changes.
 const AppOperatorWatching = "app-operator.giantswarm.io/watching"
