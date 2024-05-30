7f224591063f163de14d77d05503ae2111a3b261
Theo Brigitte <theo.brigitte@gmail.com>
2024-05-30 14:06:38 +0200

    add Flux label and annotation

---
+++ pkg/label/flux.go
@@ -0,0 +1,4 @@
+package label
+
+// FluxKustomizeName is the name of the Flux Kustomization used to apply changes to the resource holding this label.
+const FluxKustomizeName = "kustomize.toolkit.fluxcd.io/name"
